55f1f8ab2b6e3a1205e77b5062dd40afef44feff
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-12 12:52:44 -0800

    save_state pre-iteration

--- cmd/main.go
+++ cmd/main.go
@@ -9,29 +9,35 @@ import (
 	"github.com/taylormonacelli/outbow"
 )
 
-var (
-	verbose     bool
-	logFormat   string
-	storageType string
-)
+// Options struct to hold command line options
+type Options struct {
+	Verbose     bool
+	LogFormat   string
+	StorageType string
+}
 
 func main() {
-	flag.BoolVar(&verbose, "verbose", false, "Enable verbose output")
-	flag.BoolVar(&verbose, "v", false, "Enable verbose output (shorthand)")
+	// Create an instance of the Options struct
+	options := Options{}
 
-	flag.StringVar(&logFormat, "log-format", "", "Log format (text or json)")
-	flag.StringVar(&storageType, "storage", "db", "Storage type: db or json")
+	// Define command line flags and bind them to the corresponding fields in the Options struct
+	flag.BoolVar(&options.Verbose, "verbose", false, "Enable verbose output")
+	flag.BoolVar(&options.Verbose, "v", false, "Enable verbose output (shorthand)")
+
+	flag.StringVar(&options.LogFormat, "log-format", "", "Log format (text or json)")
+	flag.StringVar(&options.StorageType, "storage", "db", "Storage type: db or json")
 
 	flag.Parse()
 
-	if verbose || logFormat != "" {
-		if logFormat == "json" {
+	if options.Verbose || options.LogFormat != "" {
+		if options.LogFormat == "json" {
 			goldbug.SetDefaultLoggerJson(slog.LevelDebug)
 		} else {
 			goldbug.SetDefaultLoggerText(slog.LevelDebug)
 		}
 	}
 
-	code := outbow.Main(storageType)
+	// Pass the options struct to the Main function
+	code := outbow.Main(options.StorageType)
 	os.Exit(code)
 }
